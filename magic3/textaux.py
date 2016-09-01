96143903607be227363c58f463d38248167f8156
cypro666 <cypro666@Gmail.com>
2016-09-01 10:39:17 +0800

    cn

--- magic3/textaux.py
+++ magic3/textaux.py
@@ -114,6 +114,70 @@ awkTemplate = make_template('''awk -F "${delim}" '{print ${vargs}}' ${files} 2>&
 kvTemplate = make_template('''${k},${v}\n''')
 
 
+CN_NUM = {
+    '〇' : 0, '一' : 1, '二' : 2, '三' : 3, '四' : 4, '五' : 5, '六' : 6, '七' : 7, '八' : 8, '九' : 9, '零' : 0,
+    '壹' : 1, '贰' : 2, '叁' : 3, '肆' : 4, '伍' : 5, '陆' : 6, '柒' : 7, '捌' : 8, '玖' : 9, '貮' : 2, '两' : 2,
+}
+
+CN_UNIT = {
+    '十' : 10,
+    '拾' : 10,
+    '百' : 100,
+    '佰' : 100,
+    '千' : 1000,
+    '仟' : 1000,
+    '万' : 10000,
+    '萬' : 10000,
+    '亿' : 100000000,
+    '億' : 100000000,
+    '兆' : 1000000000000,
+}
+
+def chinese_to_arabic(cn:str) -> int:
+    lcn = list(cn)
+    unit = 0    #当前的单位
+    ldig = []   #临时数组
+
+    while lcn:
+        cndig = lcn.pop()
+        if cndig in CN_UNIT:
+            unit = CN_UNIT.get(cndig)
+            if unit == 10000:
+                ldig.append('w')
+                unit = 1
+            elif unit == 100000000:
+                ldig.append('y')
+                unit = 1
+            continue
+        else:
+            dig = CN_NUM.get(cndig)
+            if unit:
+                dig = dig*unit
+                unit = 0
+            ldig.append(dig)
+ 
+    if unit == 10:
+        ldig.append(10)
+
+    ret, tmp = 0, 0
+ 
+    while ldig:
+        x = ldig.pop()
+        if x == 'w':
+            tmp *= 10000
+            ret += tmp
+            tmp=0
+        elif x == 'y':
+            tmp *= 100000000
+            ret += tmp
+            tmp=0
+        else:
+            tmp += x
+ 
+    ret += tmp
+    return ret
+
+
 # TODO: make a full unittest
 def test():
     assert reOnlyEnglish.findall("cypro666@gmail.com") == ['cypro', 'gmail', 'com']
@@ -123,6 +187,19 @@ def test():
     assert reTime.match('05:55 PM')
     assert rePhone.match('13945678900')
     assert reEmail.match('cypro666@gmail.com')
+    test_dig = ['八',
+                '十一',
+                '一百二十三',
+                '一千二百零三',
+                '一万一千一百零一',
+                '十万零三千六百零九',
+                '一百二十三万四千五百六十七',
+                '一千一百二十三万四千五百六十七',
+                '一亿一千一百二十三万四千五百六十七',
+                '一百零二亿五千零一万零一千零三十八']
+    for cn in test_dig: 
+        x = chinese_to_arabic(cn)
+    assert x == 10250011038
 
 if __name__ == '__main__':
     test()
